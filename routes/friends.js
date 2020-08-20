a63963ddebab12ff35b42a02bc3ec4a4db8ef58c
unknown <larserlandson5@gmail.com>
2020-08-20 18:25:28 -0500

    added get pendingReuests status function to friends.js

--- routes/friends.js
+++ routes/friends.js
@@ -1,4 +1,4 @@
-const { FriendStatus, validate } = require('../models/friends');
+const { FriendStatus, validate, friendSchema } = require('../models/friends');
 const express = require('express');
 const router = express.Router();
 
@@ -12,8 +12,8 @@ router.post('/', async (req, res) => {
 
             User_id: req.body.User_id,
             Requested_by: req.body.Requested_by,
-            Online: req.body.Online,            
-            FriendStatus: req.body.FriendStatus     
+            Online: req.body.Online,
+            FriendStatus: req.body.FriendStatus
 
         });
         await friends.save();
@@ -22,6 +22,17 @@ router.post('/', async (req, res) => {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
+
+router.get('/pendingRequests/:Requested_by/:FriendStatus', async (req, res) => {
+
+    try {
+        const friendRequest = await FriendStatus.find({ "Requested_by": req.params.Requested_by, "FriendStatus": req.params.FriendStatus });
+        return res.send(friendRequest);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+
+})
 
 
 module.exports = router;
