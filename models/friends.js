39655f5c1b73791983a52de459c48af6815b1571
unknown <larserlandson5@gmail.com>
2020-08-19 20:41:01 -0500

    added friendSchema to include userid, online and friendstatus.

---
+++ models/friends.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const friendSchema = new mongoose.Schema({
+    User_id:        { type: Number, required: true},
+    Online:         {type: Boolean, required: true},
+    FriendStatus:   {type: String, required: true}
+})
+
+
