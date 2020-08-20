f02ac8b61151968c5cd49ea638e4b05a3174942f
unknown <larserlandson5@gmail.com>
2020-08-19 21:16:51 -0500

    added post to friends.js file

--- models/friends.js
+++ models/friends.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const friendSchema = new mongoose.Schema({
-    User_id:        { type: Number, required: true},
-    Online:         {type: Boolean, required: true},
-    FriendStatus:   {type: String, required: true}
+    User_id: { type: Number, required: true },
+    Online: { type: Boolean, required: true },
+    FriendStatus: { type: String, required: true }
 })
 
 const FriendStatus = mongoose.model('FriendStatus', friendSchema);
---
+++ routes/friends.js
@@ -0,0 +1,26 @@
+const { FriendStatus, validate } = require('../models/friends');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error)
+            return res.status(400).send(error);
+
+        const friends = new FriendStatus({
+
+            User_id: req.body.User_id,
+            // Online: { type: Boolean, required: true },
+            // FriendStatus: { type: String, required: true }
+
+        });
+        await friends.save();
+        return res.send(friends);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+
+module.exports = router;
