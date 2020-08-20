102e7121bade30d77dfebb3c75125bf517d2bcac
unknown <kylespinaio@hotmail.com>
2020-08-20 19:40:38 -0400

    inital authorization route and validation has been added. tested in postman. returns token as designed.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
 const user = require('./routes/users')
+const auth = require('./routes/auth')
+
 
 var cors = require('cors');
 app.use(cors());
@@ -9,7 +11,7 @@ app.use(cors());
 connectDB();
 app.use(express.json());
 app.use("/api/users", user);
-
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
