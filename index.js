cc9710def66decd153d26503a9ace67cffc28dc4
david rubio <rubdo@davids-Air.attlocal.net>
2020-08-20 19:22:29 -0500

    updated added posts and added likes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
-const comments = require('./routes/comments');
+const posts = require('./routes/posts');
 
 var cors = require('cors');
 app.use(cors());
 
 connectDB();
 app.use(express.json());
-app.use('/api/comments', comments);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
